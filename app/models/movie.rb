8e0746da8e6b3c8aff9dd9464b37d5e23210fb31
DEEPAKRAJ DHARMAPURI SELVAKUMAR <dd3068@columbia.edu>
2021-02-04 09:51:44 +0000

    Minor corrections

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,18 +1,17 @@
 class Movie < ActiveRecord::Base
-	def initialize
-		@all_ratings = ['G','PG','PG-13','R']
-	end
 
-	def all_ratings
-		@all_ratings
+	def self.all_ratings
+		#	['G','PG','PG-13','R']
+		@@all_ratings = ['G','PG','PG-13','R']
+		@@all_ratings	
 	end
 
 	def self.with_ratings(ratings_list)
 		filtered_movies = nil
 		if ratings_list == nil
-			filtered_movies = @movies.all
+			filtered_movies = self.all
 		else
-			filtered_movies = @movies.where(rating: ratings_list)
+			filtered_movies = self.where(rating: ratings_list)
 		end
 		filtered_movies
 	end
