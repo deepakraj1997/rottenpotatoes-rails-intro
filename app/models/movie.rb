659f498fc5313776be9b8180be8678123960cfdd
DEEPAKRAJ DHARMAPURI SELVAKUMAR <dd3068@columbia.edu>
2021-02-04 09:41:53 +0000

    Adding part 1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,19 @@
 class Movie < ActiveRecord::Base
+	def initialize
+		@all_ratings = ['G','PG','PG-13','R']
+	end
+
+	def all_ratings
+		@all_ratings
+	end
+
+	def self.with_ratings(ratings_list)
+		filtered_movies = nil
+		if ratings_list == nil
+			filtered_movies = @movies.all
+		else
+			filtered_movies = @movies.where(rating: ratings_list)
+		end
+		filtered_movies
+	end
 end
